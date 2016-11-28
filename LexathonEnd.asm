e49a8f2212e5b9d8558ed64ec8c0cd6264360dcf
axc130230 <axc130230@utdallas.edu>
2016-11-28 11:02:31 -0600

    Update LexathonEnd.asm

--- LexathonEnd.asm
+++ LexathonEnd.asm
@@ -2,10 +2,7 @@
 	gameOver		.asciiz "GAME OVER\n"
 	finalScore		.asciiz "FINAL SCORE:\n"
 	numberCorrectWords 	.asciiz "NUMBER OF WORDS FOUND:\n"
-	correctWordsList 	.asciiz "WORDS FOUND:\n"
 	numberMissedWords 	.asciiz "NUMBER OF WORDS MISSED:\n"
-	numberTotalWords 	.asciiz "NUMBER OF POSSIBLE WORDS:\n"
-	totalWordsList 		.asciiz "POSSIBLE WORDS:\n"
 	tryAgain 		.asciiz "TRY AGAIN? ('1' = YES, '0' = NO): "
 	
 .text
@@ -37,19 +34,7 @@
 		li $v0, 4			#skip line
 		la $a0, newLine
 		syscall
-		
-		li $v0, 4			#prints "WORDS FOUND: " message
-		la $a0, correctWordsList
-		syscall
-		
-		li $v0, 4			#list of found words
-		la $a0, correctWords
-		syscall
-		
-		li $v0, 4			#skip line
-		la $a0, newLine
-		syscall
-		
+				
 	showTotalWords:
 		li $v0, 4			#prints "NUMBER OF WORDS MISSED: " message
 		la $v0, numberMissedWords
@@ -65,35 +50,4 @@
 		la $a0, newLine
 		syscall
 		
-		li $v0, 4			#prints "NUMBER OF POSSIBLE WORDS: " message
-		la $a0, numberTotalWords
-		syscall
 		
-		li $v0, 1			##prints total number of words
-		move $a0, $s1			#moving total words for printing
-		syscall
-		
-		li $v0, 4			#skip line
-		la $a0, newLine
-		syscall
-		
-		li $v0, 4			#prints "POSSIBLE WORDS: " message
-		la $a0, totalWordsList
-		syscall
-		
-		li $v0, 4			#skip line
-		la $a0, newLine
-		syscall
-		
-	showPossibleWords:
-		add $t1, $zero, $zero
-		
-	showPossibleWordsLoop:
-		li $v0, 4
-		la $a0, allowedWords
-		syscall
-		
-		la $a0, newLine			#skip line
-		syscall
-		
-		j endGame
