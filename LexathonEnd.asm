8e72c0b1276cde8df990b18d123b1df1c40d0e3e
axc130230 <axc130230@utdallas.edu>
2016-11-30 22:19:14 -0600

    Update LexathonEnd.asm

--- LexathonEnd.asm
+++ LexathonEnd.asm
@@ -55,6 +55,28 @@ fillword:	#PRINT THE PUZZLE
 		syscall					#get the word
 		
 		#A GOOD PLACE TO CHECK TO SEE IF TIME IS UP
+timer:
+	li $v0, 30
+	syscall
+	
+	addu $s2, $a0, $zero	#store starting time
+
+	addiu $s2, $s2, 60000
+	
+	start:	
+
+		li $v0, 30
+		syscall
+
+		addu $t0, $a0, $zero 	#store changed time
+		
+		subu $t0, $s2, $t0
+
+		bltz $t0, end
+
+		j start
+
+	end:
 		
 		#This section eliminates the newline character in the word entered by the user
 		li $t0, 0				#sets the iterator for nonewline
